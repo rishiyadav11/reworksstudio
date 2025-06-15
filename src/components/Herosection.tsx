b69f28be1a869cb8d1a99b2e23e34f689029b6f6
Rishi yadav <155768790+rishiyadav11@users.noreply.github.com>
2025-06-15 23:21:07 +0530

    Update Herosection.tsx aaded h-screen

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -3,7 +3,7 @@ import { BackgroundLines } from "./ui/background-lines";
 
 export function Herosection() {
   return (
-    <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+    <BackgroundLines className="flex items-center justify-center min-h-screen w-full flex-col px-4">
       <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-600 to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
         Reworks Studio, <br /> Crafting Visual Brilliance.
       </h2>
